8c24dd1e85342692b20d3028da7d3bee96df23c1
Aliaksandr Arefyeu <a@godigitale.com>
2020-06-28 13:50:18 +0300

    Create bg-facebarone.js

---
+++ bg-facebarone.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    document.getElementsByClassName('breadcrumb-blog')[0].style.backgroundImage = "url(https://shop.facebarone.com/wp-content/uploads/2020/04/ABOUT_US-min.png)";
+    document.getElementsByClassName('breadcrumb-blog')[0].style.backgroundRepeat = "no-repeat";
+    document.getElementsByClassName('breadcrumb-blog')[0].style.backgroundSize = "contain";
+    document.getElementsByClassName('breadcrumb-blog')[0].style.backgroundAttachment = "scroll";
+    document.getElementsByClassName('breadcrumb-blog')[0].style.backgroundPosition = "center";
+}
+
+
+
